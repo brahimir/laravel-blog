0cd84cf5b7e1e28572b0f81c05dbe1a82f00469c
brahimir <nickb@schemaapp.com>
2024-05-08 13:07:14 -0400

    lesson-9

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post Info</title>
+</head>
+
+<body>
+    <article>
+        <h1>
+            Post #1
+        </h1>
+
+        <?= $post ?>
+
+        <a href="/">Go Back</a>
+    </article>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('posts/{post}', function ($slug) {
+    $post = file_get_contents(__DIR__ . "/../resources/posts/${slug}.html");
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
